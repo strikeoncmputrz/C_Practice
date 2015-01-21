7ad79371e758e009823d889c432db6588af83497
Ernest <ernestrrussell@gmail.com>
2015-01-21 18:22:30 -0500

    Added 8.2

---
+++ Chapter8/8-2_Tomorrows_Date.c
@@ -0,0 +1,42 @@
+//Program for determining tomorrow's date
+
+#include <stdio.h>
+
+int main (void)
+{
+	struct date
+	{
+		int month, day, year;
+	};
+
+	struct date tomorrow, today;
+
+	const int daysPerMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	printf("Enter today's date (mm dd yyyy): ");
+	scanf("%i%i%i", &today.month, &today.day, &today.year);
+
+	if (today.day != daysPerMonth[today.month -1] )
+	{
+		tomorrow.day = today.day + 1;
+		tomorrow.month = today.month;
+		tomorrow.year = today.year;
+	}
+	else if (today.month == 12) //end of year
+	{
+		tomorrow.day = 1;
+		tomorrow.month = 1; 
+		tomorrow.year = today.year + 1;
+	}
+	else 			     //end of month
+	{
+		tomorrow.day = 1;
+		tomorrow.month = today.month + 1;
+		tomorrow.year = today.year;
+	}
+
+	printf("Tomorrow's date is %i/%i/%.2i.\n", tomorrow.month, tomorrow.day, tomorrow.year % 100);
+
+	return 0;
+
+}
