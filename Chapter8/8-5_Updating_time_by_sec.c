92aac4f06417f7c9dff7f49f1da482c657790184
Ernest <ernestrrussell@gmail.com>
2015-01-22 19:41:35 -0500

    Added 8-5

---
+++ Chapter8/8-5_Updating_time_by_sec.c
@@ -0,0 +1,49 @@
+// Program to update the Time by One Second
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct time
+{
+	int hour, min, sec;
+};
+
+int main (void)
+{
+	struct time timeUpdate (struct time now);
+	struct time currentTime, nextTime;
+
+	printf("Enter the time (hh:mm:ss) ");
+	scanf ("%i:%i:%i", &currentTime.hour, &currentTime.min, &currentTime.sec);
+
+	nextTime = timeUpdate (currentTime);
+
+	printf("Updated time is %.2i:%.2i:%.2i\n", nextTime.hour,nextTime.min, nextTime.sec);
+
+	return 0;
+}
+
+//Function to update the time by one second
+
+struct time timeUpdate (struct time now)
+{
+	++now.sec;
+	
+	if(now.sec == 60)		//next minute
+	{
+		now.sec = 0;
+		++now.min;
+
+		if(now.min == 60) 	 //next hour
+		{	
+			now.min = 0;
+			++now.hour;
+
+			if(now.hour == 24) //midnight
+			{
+				now.hour = 0;
+			}
+		}
+	}
+	return now;
+}
