15ca1a09178eb100b5c39f68fc952f3d639165f8
Ernest <ernestrrussell@gmail.com>
2015-02-02 23:23:31 -0500

    got ex9-4 with VOID

---
+++ Chapter9/ex9-4_Substring.c
@@ -0,0 +1,47 @@
+//Function to return a substing
+
+#include <stdio.h>
+
+//Creates a substing from source and puts it in result
+void subString(const char source[], int start, int count, char result[])
+{
+	int length = stringLength(source);
+	int i;
+	printf("%i\n", count);
+	count = length%count;
+	printf("%i\n", count);
+	//Cycle through source from start to start + count -1
+	for(i = 0; i < count; ++i)
+	{
+		result[i] = source[start];
+		++start;
+	}
+	result[i+1] = '\0';
+
+
+}
+
+//Determines a string's length
+int stringLength(const char string[])
+{
+	int count = 0;
+
+	while( string[count] != '\0')
+		++count;
+
+	return count;
+}
+
+int main (void)
+{
+//	int stringLength(const char string[]);
+	void subString(const char source[], int start, int count, char result[]);
+	char result[50];
+	//subString("character", 4, 3, result);
+	//printf("%s\n", result);
+	subString("two words", 4, 200, result);
+	printf("%s\n", result);
+
+	
+	return 0;
+}
