a31592dca04b73bd74bb4ae47f7349c05f65ca94
Ernest <ernestrrussell@gmail.com>
2015-02-08 15:27:37 -0500

    Added 10-11 through 13

---
+++ Chapter10/10-13_Pointer_version_of_CopyString.c
@@ -0,0 +1,29 @@
+//Funciton to copy contents of one string to another using pointers
+
+#include <stdio.h>
+
+void copyString(char to[], char from[])
+{
+	int i;
+
+	for( ; *from != '\0'; ++from, ++to)
+		*to = *from;
+	*to = '\0';
+}
+
+int main (void)
+{
+
+	void copyString(char *to, char *from);
+	char string1[] = "A string to be copied";
+	char string2[50];
+
+	copyString(string2, string1);
+	printf("%s\n", string2);
+
+	copyString(string2, "So is this.");
+	printf("%s\n", string2);
+
+	return 0;
+
+}
