51b714011082300431cec1aa9d64f86285ea0950
Ernest <ernestrrussell@gmail.com>
2015-08-28 10:04:15 -0400

    Fixed ex11-2

---
+++ Chapter11/11-2_Bitwise_AND.c
@@ -0,0 +1,13 @@
+//Program to determine how this PC implements bitwise right shift
+
+#include <stdio.h>
+
+int main (void)
+{
+	int w1 = -128;
+	printf("%i \n", w1);
+	printf("%i \n", w1 >> 1);
+
+	return 0;
+
+};
