bb6eb05d83c9081d2b0b825d35cee56aedbc73db
Ernest <ernestrrussell@gmail.com>
2015-09-21 20:29:40 -0400

    Started Chapter 13

---
+++ Chapter13/13-1_Using_Enumerated_Data_Types.c
@@ -0,0 +1,46 @@
+//This program illustrated enumerated data types
+//It displays the number of days in a month
+
+#include <stdio.h>
+
+int main (void)
+{
+
+	enum month {January = 1, February, March, April, May, June, July, August, September, October, November, December };
+	enum month aMonth;
+	int days;
+
+	printf("Enter a month number: ");
+	scanf("%i", &aMonth);
+
+	switch(aMonth)
+	{
+		case January:
+		case May:
+		case July:
+		case August:
+		case October:
+		case December:
+			days = 31;
+			break;
+		case April:
+		case June:
+		case September:
+		case November:
+			days = 30;
+			break;
+		case February:
+			days = 28;
+			break;
+		default:
+			printf("bad month number\n");
+			days = 0;
+			break;
+	}
+
+	if(days != 0)
+		printf("Number of days is %i\n", days);
+	if(aMonth == February)
+		printf("...or 29 if it's a leap year\n");
+	return 0;
+}
